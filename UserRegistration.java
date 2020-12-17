5c22d1535de13eda7f463bf0e1165356754ceb67
Rajas1312 <you@example.com>
2020-12-17 16:44:55 +0530

    Added: code to validate first name

--- UserRegistration.java
+++ UserRegistration.java
@@ -7,6 +7,11 @@ public class UserRegistration {
 
 	public static void main(String[] args) {
 		System.out.println("welcome to user registration");
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("Enter the first name");
+		String firstName=scanner.next();
+		System.out.println(Pattern.matches("[A-Z]{1}[a-z]{2,}",firstName));
+
 	}
 
 }
