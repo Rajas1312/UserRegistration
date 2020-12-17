d8dfa2eeb20e92058d1a0eee7e1abab39fac84d9
Rajas1312 <you@example.com>
2020-12-17 17:04:44 +0530

    Added: code to validate last name

--- UserRegistration.java
+++ UserRegistration.java
@@ -4,13 +4,37 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
+	
+	Scanner scanner=new Scanner(System.in);
 
+	public void checkFirstName() {
+		System.out.println("Enter the first name");
+		boolean firstName=Pattern.matches("[A-Z]{1}[a-z]{2,}",scanner.next());
+		if(firstName==true) {
+			System.out.println("valid firstname");
+		}
+		else {
+			System.out.println("invalid firstname");
+		}
+	}
+	public void checkLastName() {
+		System.out.println("Enter the last name");
+		boolean lastName=Pattern.matches("[A-Z]{1}[a-z]{2,}",scanner.next());
+		if(lastName==true) {
+			System.out.println("valid lastname");
+		}
+		else {
+			System.out.println("invalid lastname");
+		}
+	}
+	
+	
 	public static void main(String[] args) {
 		System.out.println("welcome to user registration");
-		Scanner scanner=new Scanner(System.in);
-		System.out.println("Enter the first name");
-		String firstName=scanner.next();
-		System.out.println(Pattern.matches("[A-Z]{1}[a-z]{2,}",firstName));
+		
+		UserRegistration userRegistration=new UserRegistration();
+		userRegistration.checkFirstName();
+		userRegistration.checkLastName();
 
 	}
 
