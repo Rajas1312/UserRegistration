c24763631ce6aa3b85dbef7307c26261c440c1b3
Rajas1312 <you@example.com>
2020-12-17 16:39:05 +0530

    Added: welcome messgae

---
+++ UserRegistration.java
@@ -0,0 +1,12 @@
+package RegexDemo;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class UserRegistration {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to user registration");
+	}
+
+}
